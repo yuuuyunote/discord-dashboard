223cae9ab7cf82d8eb0880ae461053b11e9bf78e
ゆううゆ <183489596+yuuuyunote@users.noreply.github.com>
2026-08-24 11:05:55 +0900

    Rename 'dm-solicitation' to 'bad-solicitation'

--- bot/commands/categories.py
+++ bot/commands/categories.py
@@ -10,7 +10,7 @@ CATEGORY_LABELS = {
     "phishing": "フィッシング",
     "impersonation": "なりすまし",
     "raid-spam": "荒らし・スパム",
-    "dm-solicitation": "悪質な勧誘",
+    "bad-solicitation": "悪質な勧誘",
     "harassment": "嫌がらせ",
     "doxxing": "個人情報の暴露",
     "hate-speech": "差別的言動",
